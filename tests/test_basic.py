8acc0ada61467a5f77bc6c3949196c0ff0c1d3ad
Andy Shapiro <shapiromatron@gmail.com>
2025-08-09 00:31:37 -0400

    add eigen

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import demo
 from demo import cppcore
 
@@ -6,3 +8,11 @@ def test_main():
     assert demo.__version__ == "0.0.1"
     assert cppcore.add(1, 2) == 3
     assert cppcore.subtract(1, 2) == -1
+
+
+def test_eigen_matmul():
+    a = np.array([[1.0, 2.0], [3.0, 4.0]])
+    b = np.array([[5.0, 6.0], [7.0, 8.0]])
+    result = cppcore.eigen_matmul(a, b)
+    expected = np.matmul(a, b)
+    np.testing.assert_allclose(result, expected)
