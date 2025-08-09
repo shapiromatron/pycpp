997c51fc97a0be07a8743c692106c266a330878d
Andy Shapiro <shapiromatron@gmail.com>
2025-08-08 21:56:57 -0400

    continue

--- tools/cibw_linux_before_all.sh
+++ tools/cibw_linux_before_all.sh
@@ -2,5 +2,5 @@ pwd
 
 ls -laht
 
-pip install pybind11 --target=/project/pybind11
+pip install pybind11==3.0.0 --target=/project/pybind11
 export CMAKE_PREFIX_PATH=/project/pybind11/pybind11/share/cmake
