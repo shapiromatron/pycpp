ea4d2f7d4bf5c027140ce6adfb9cbd9cc6a4177b
Andy Shapiro <shapiromatron@gmail.com>
2025-08-10 13:05:26 -0400

    continue

--- tools/cibw_before_linux.sh
+++ tools/cibw_before_linux.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-yum install zip
-
 set -eo pipefail
 
-pip install pybind11==3.0.0 --target=./pybind11
+if [ "${RUNNER_OS}" = "Linux" ]; then
+    yum install -y zip
+fi
 
 echo $VCPKG_HOST_TRIPLET
 ./vcpkg/bootstrap-vcpkg.sh
 ./vcpkg/vcpkg.exe install --host-triplet=$VCPKG_HOST_TRIPLET
+
+pip install pybind11==3.0.0 --target=./pybind11
