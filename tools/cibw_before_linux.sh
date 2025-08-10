ca90a89359daef748ed7a3a39bc23201bfb096c2
Andy Shapiro <shapiromatron@gmail.com>
2025-08-10 12:59:01 -0400

    progress

--- tools/cibw_before_linux.sh
+++ tools/cibw_before_linux.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+yum install zip
+
 set -eo pipefail
 
 pip install pybind11==3.0.0 --target=./pybind11
 
 echo $VCPKG_HOST_TRIPLET
 ./vcpkg/bootstrap-vcpkg.sh
-./vcpkg/vcpkg install --host-triplet=$VCPKG_HOST_TRIPLET
+./vcpkg/vcpkg.exe install --host-triplet=$VCPKG_HOST_TRIPLET
--- tools/cibw_before_windows.sh
+++ tools/cibw_before_windows.sh
@@ -4,15 +4,8 @@ set -eo pipefail
 
 pip install pybind11==3.0.0 --target=$GITHUB_WORKSPACE/pybind11
 
-pwd
-ls
+echo $VCPKG_HOST_TRIPLET
+./vcpkg/bootstrap-vcpkg.sh
+./vcpkg/vcpkg install --host-triplet=$VCPKG_HOST_TRIPLET
 
 echo $GITHUB_WORKSPACE
-
-ls -laht $GITHUB_WORKSPACE
-ls -laht $GITHUB_WORKSPACE/pybind11
-ls -laht $GITHUB_WORKSPACE/pybind11/pybind11
-ls -laht $GITHUB_WORKSPACE/pybind11/pybind11/share
-ls -laht $GITHUB_WORKSPACE/pybind11/pybind11/share/cmake
-ls -laht $GITHUB_WORKSPACE/pybind11/pybind11/share/cmake/pybind11
-
