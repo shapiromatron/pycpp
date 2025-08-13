547efbfecda93433d0d4aa09b37e5b1c7bc67b2d
Andy Shapiro <shapiromatron@gmail.com>
2025-08-12 21:47:27 -0400

    cleanup cpp

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -5,18 +5,15 @@
 #include <Eigen/Dense>
 #include <nlopt.h>
 
-#define STRINGIFY(x) #x
-#define MACRO_STRINGIFY(x) STRINGIFY(x)
-
 int add(int i, int j) {
     return i + j;
 }
 
 namespace py = pybind11;
 
-// EIGEN test
+// EIGEN
 py::array_t<double> eigen_matmul(py::array_t<double, py::array::c_style | py::array::forcecast> a,
-                           py::array_t<double, py::array::c_style | py::array::forcecast> b) {
+                                 py::array_t<double, py::array::c_style | py::array::forcecast> b) {
     if (a.ndim() != 2 || b.ndim() != 2)
         throw std::runtime_error("Input must be two 2D arrays");
     if (a.shape(1) != b.shape(0))
@@ -31,25 +28,24 @@ py::array_t<double> eigen_matmul(py::array_t<double, py::array::c_style | py::ar
     return py::array_t<double>(
         {C.rows(), C.cols()},
         {sizeof(double) * C.cols(), sizeof(double)},
-        C.data()
-    );
+        C.data());
 }
 
-// GSL test
+// GSL
 double gsl_bessel(double x) {
     return gsl_sf_bessel_J0(x);
 }
 
-// NLOPT test
-double objfunc(unsigned n, const double* x, double* grad, void*) {
+// NLOPT
+double objfunc(unsigned n, const double *x, double *grad, void *) {
     if (grad) {
         grad[0] = 2 * (x[0] - 1);
         grad[1] = 2 * (x[1] - 2);
     }
     return (x[0] - 1) * (x[0] - 1) + (x[1] - 2) * (x[1] - 2);
 }
-std::vector<double> nlopt_demo(const std::vector<double>& lower_bounds,
-                              const std::vector<double>& upper_bounds) {
+std::vector<double> nlopt_demo(const std::vector<double> &lower_bounds,
+                               const std::vector<double> &upper_bounds) {
     if (lower_bounds.size() != 2 || upper_bounds.size() != 2)
         throw std::invalid_argument("Bounds must have size 2");
 
@@ -62,39 +58,16 @@ std::vector<double> nlopt_demo(const std::vector<double>& lower_bounds,
     double minf;
     int result = nlopt_optimize(opt, x, &minf);
     nlopt_destroy(opt);
-    if (result < 0) throw std::runtime_error("nlopt failed");
+    if (result < 0)
+        throw std::runtime_error("nlopt failed");
     return {x[0], x[1]};
 }
 
+// Python module
 PYBIND11_MODULE(cppcore, m) {
-    m.doc() = R"pbdoc(
-        Pybind11 example plugin
-        -----------------------
-
-        .. currentmodule:: cppcore
-
-        .. autosummary::
-           :toctree: _generate
-
-           add
-           subtract
-    )pbdoc";
-
-    m.def("add", &add, R"pbdoc(
-        Add two numbers
-
-        Some other explanation about the add function.
-    )pbdoc");
-
-    m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
-        Subtract two numbers
-
-        Some other explanation about the subtract function.
-    )pbdoc");
-
+    m.def("add", &add, "Add two numbers");
     m.def("eigen_matmul", &eigen_matmul, "Matrix multiplication using Eigen");
     m.def("gsl_bessel", &gsl_bessel, "Bessel function using GSL");
-    m.def("nlopt_optimize", &nlopt_demo,
-        py::arg("lower_bounds"), py::arg("upper_bounds"),
-        "Run NLOPT optimization with specified bounds");
+    m.def("nlopt_optimize", &nlopt_demo, py::arg("lower_bounds"), py::arg("upper_bounds"),
+          "Run NLOPT optimization with specified bounds");
 }
